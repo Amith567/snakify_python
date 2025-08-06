8b08fd9e47e1f28a500dc02cb0d2295b8bdbb432
Amith P <amithanilp@gmail.com>
2025-08-07 00:18:47 +0530

    1.4 complete

---
+++ 1/1_4.py
@@ -0,0 +1,6 @@
+# Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a separate line.
+
+
+width=int(input())
+height=int(input())
+print(0.5*width*height)
