7532d2c3ba5008f737cd79a3d354fe940e789839
Amith P <amithanilp@gmail.com>
2025-08-11 19:29:35 +0530

    1.7 complete

--- 1/1_7.py
+++ 1/1_7.py
@@ -1,3 +1,5 @@
 # Write a program that reads an integer number and prints its previous and next numbers. See the examples below for the exact format your answers should take. There shouldn't be a space before the period.
 
-abhinav 
+number=int(input())
+print("The next number for the number",number,"is {}.".format(number+1))
+print("The previous number for the number",number," is {}.".format(number-1))
