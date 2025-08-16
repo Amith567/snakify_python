302608501820ea98a0d5494bc9158418e86729b9
Amith P <amithanilp@gmail.com>
2025-08-16 19:28:43 +0530

    complted

---
+++ 3)Conditions if, then, else/Rook move.py
@@ -0,0 +1,20 @@
+# #Chess rook moves horizontally or vertically. Given two different cells of the chessboard, determine whether a rook can go from the first cell to the second in one move.
+# The program receives the input of four numbers from 1 to 8, each specifying the column and row number, first two - for the first cell, and then the last two - for the second cell. The program should output YES if a rook can go from the first cell to the second in one move, or NO otherwise.
+
+
+initial_x=int(input())
+initial_y=int(input())
+final_x=int(input())
+final_y=int(input())
+if initial_x<final_x:
+    if initial_y>final_y or initial_y<final_y:
+        print("NO")
+    else:
+        print("YES")
+elif initial_x==final_x:
+    print("YES")
+else:
+        if initial_y>final_y or initial_y<final_y:
+            print("NO")
+        else:
+            print("YES")
